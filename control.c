d801ddb892a10d08784e208d18a6b30f98a24d8b
jennisun <jsun20@stuy.edu>
2021-12-06 21:14:20 -0500

    wrote basic outline

---
+++ control.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <sys/ipc.h>
+#include <sys/sem.h>
+#include <sys/types.h>
+#include <errno.h>
+
+#define KEY1 24601
+#define KEY2 24602
+
+
+int int main(int argc, char const *argv[]) {
+  if (argc < 2) printf("Not enough arguments\n");
+  else if (strcmp(argc[i], "create") == 0) {
+
+  }
+  else if (strcmp(argc[i], "create") == 0) {
+
+  }
+  else printf("Invalid arguments\n");
+
+  return 0;
+}
